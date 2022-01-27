fa4bb13b5ece83a76433c19827517e361d9bea1e
ManGuevara <96947666+ManGuevara@users.noreply.github.com>
2022-01-27 07:25:41 -0500

    Update object_player.py

--- object_player.py
+++ object_player.py
@@ -12,16 +12,16 @@
 
     Attributes:
         
-        desition (boolean L/H): the player decide if the turn is for Low or for High card value.
+        guessing (boolean L/H): the player decide if the turn is for Low or for High card value.
         outcomes (List[win, lose]): depending of the outcomes.
-        earn points (int): The points earned for one round of play.
-        lose points (int) The points lost for one round of play.
+        earn points (int): The points earned if the guess is correct for one round of play.
+        lose points (int) The points lost if the guess is incorrect for one round of play.
         total_score (int): The score for the player in the entire game.
         finish the game (boolean): the player decide Y/N to play.
 
     Methods names:
         def ask_for_a_card(self)
-        def desition(self)
+        def guessing(self)
         def decide_to_continue_playing(self)
         def close(self)
 
